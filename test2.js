7940cf2c1594c0355d0942ab00f28c122da0eeb9
suryanshhh445 <suryanshrajsingh54@gmail.com>
2025-10-16 15:52:07 +0530

    added basic variable

--- test2.js
+++ test2.js
@@ -1,1 +1,12 @@
-console.log("HI I am suryansh and this is test")
+const accountId = 123456
+let accountEmail = "suryansh.24848@kiet.edu"
+var accountpassword = "Suryansh"
+accountCity = "Bareilly"
+
+let accountState;
+
+accountCity ="Bengaluru"
+accountEmail = "suryanshjkkj@gmail.com"
+accountPassword = "Suryansh2005"
+
+console.table([accountId, accountEmail, accountCity,accountState])
